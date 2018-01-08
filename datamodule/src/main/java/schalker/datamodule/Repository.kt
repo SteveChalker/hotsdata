a64c30881a786b3a1b6b507ac72f700b547d93fa
Stephen Chalker <stephen@bypassmobile.com>
2018-01-08 10:30:28 -0600

    add comments to remind myself on what to do

--- datamodule/src/main/java/schalker/datamodule/Repository.kt
+++ datamodule/src/main/java/schalker/datamodule/Repository.kt
@@ -14,4 +14,9 @@ class Repository @Inject constructor(var hotsService: HotsService, var heroesDao
 
     fun refreshData() = hotsService.getHeroes().subscribeOn(Schedulers.io())
             .subscribe{response -> heroesDao.insertHeroes(response)}
+    //Add ignored fieled to entity for list of abilities and talents
+    //create entities for abilities and talents
+    //on respoonse insert abilities
+    //on response add owner name to talents object and then insert
+
 }
