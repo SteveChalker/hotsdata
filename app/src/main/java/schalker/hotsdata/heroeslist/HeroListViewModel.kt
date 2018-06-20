fb5215ba61fa6d0aadeb4f1ff6a5c3a2524b6139
Unknown <srchalker25@gmail.com>
2018-06-19 22:29:19 -0500

    Rename MainActivity and move HeroListViewModel

---
+++ app/src/main/java/schalker/hotsdata/heroeslist/HeroListViewModel.kt
@@ -0,0 +1,15 @@
+package schalker.hotsdata.heroeslist
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.ViewModel
+import schalker.datamodule.Repository
+import schalker.datamodule.models.Hero
+import javax.inject.Inject
+
+class HeroListViewModel @Inject constructor(val repository: Repository) : ViewModel() {
+    val heroes: LiveData<List<Hero>> = repository.getHeroes()
+
+    fun fetchHeroes() {
+        repository.refreshData()
+    }
+}
